545646be890e677efc3dc288c0fb1b47a13f7686
Marco Arélio <testingmarco@outlook.com>
2022-08-07 18:34:17 -0300

    alterações no teste Automatizado

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,38 @@
+describe('empty spec', () => {
+ 
+
+    beforeEach(() => {
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+      cy.screenshot()
+    });
+    
+    it('Deve fazer login com sucesso', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste_aluno20 (não é teste_aluno20? Sair)')
+    
+  })
+
+  it('Deve exibir uma mensagem de erro ao insserir usuário inválido', () => {
+    cy.get('#username').type('3porcrito@.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error').should('contain', 'Erro: O usuário 3porcrito@.com não está registrado neste site.')
+  })
+
+  
+  it('Deve exibir uma mensagem de erro ao inspecionar senha inválida', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+
+  })
+    
+})
---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,31 @@
+describe('Funcionalidade Página de produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/page/3/')
+    });
+    
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+              
+    });
+
+    it.only('Deve adiconar um produto ao carrinho', () => {
+        
+        var quantidade = 10
+        
+        cy.get('[class="product-block grid"]')
+            .contains('Augusta Pullover Jacket').click()    
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Augusta Pullover Jacket” foram adicionados no seu carrinho.')
+    });
+})
